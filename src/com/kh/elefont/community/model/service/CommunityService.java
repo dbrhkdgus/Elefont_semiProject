0da78d636cec9ce5c6a81129f844517c622ecc7d
ehqkr <ehqkr@LAPTOP-1G18ANL7>
2021-10-25 10:46:30 +0900

    유광현 - 커뮤니티 게시물 등록 기능 구현 중

--- src/com/kh/elefont/community/model/service/CommunityService.java
+++ src/com/kh/elefont/community/model/service/CommunityService.java
@@ -1,5 +1,24 @@
 package com.kh.elefont.community.model.service;
 
+import static com.kh.elefont.common.JdbcTemplate.close;
+import static com.kh.elefont.common.JdbcTemplate.getConnection;
+import static com.kh.elefont.common.JdbcTemplate.commit;
+import static com.kh.elefont.common.JdbcTemplate.rollback;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.elefont.community.model.dao.CommunityDao;
+
+
 public class CommunityService {
+	CommunityDao communityDao = new CommunityDao();
+	public int enrollBoard() {
+		Connection conn = getConnection();
+		int result = communityDao.enrollBoard(conn, );
+		close(conn);
+
+		return 0;
+	}
 
 }
--- src/com/kh/elefont/community/model/vo/Commnity.java
+++ src/com/kh/elefont/community/model/vo/Commnity.java
@@ -1,5 +1,5 @@
 package com.kh.elefont.community.model.vo;
 
 public class Commnity {
-
+	
 }
