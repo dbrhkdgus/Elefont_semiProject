dbc1cbf45d0ce749334ae39627035a1958779fcf
wattk <watt.kwon@gmail.com>
2021-10-25 16:11:56 +0900

    권혜진 - 판매자 마이페이지에서 폰트 등록 기능 구현 25일 16:11

--- src/com/kh/elefont/font/model/dao/FontDao.java
+++ src/com/kh/elefont/font/model/dao/FontDao.java
@@ -1,5 +1,98 @@
 package com.kh.elefont.font.model.dao;
 
+import static com.kh.elefont.common.JdbcTemplate.close;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.kh.elefont.common.model.vo.Attachment;
+import com.kh.elefont.font.model.vo.Font;
+
 public class FontDao {
+	private Properties prop = new Properties();
+
+	public FontDao() {
+		String filepath = FontDao.class.getResource("/font/font-query.properties").getPath();
+		
+		try {
+			prop.load(new FileReader(filepath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int insertFont(Connection conn, Font font) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String sql = prop.getProperty("insertFont");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, font.getFontName());
+			pstmt.setString(2, font.getFontUrl());
+			pstmt.setDouble(3, font.getFontPrice());
+			pstmt.setString(4, font.getMemberId());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	public String selectLastFontNo(Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectLastFontNo");
+		String fontNo = "";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				fontNo = rset.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return fontNo;
+	}
+
+	public int insertAttachment(Connection conn, Attachment attach) {
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("insertAttachmentFromFont");
+		int result = 0;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, attach.getMemberNo());
+			pstmt.setString(2, attach.getOriginalFilename());
+			pstmt.setString(3, attach.getRenamedFilename());
+			pstmt.setString(4, attach.getFontNo());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		return result;
+	}
 
 }
--- src/com/kh/elefont/font/model/service/FontService.java
+++ src/com/kh/elefont/font/model/service/FontService.java
@@ -1,5 +1,42 @@
 package com.kh.elefont.font.model.service;
 
+import static com.kh.elefont.common.JdbcTemplate.*;
+
+import java.sql.Connection;
+
+import com.kh.elefont.common.model.vo.Attachment;
+import com.kh.elefont.font.model.dao.FontDao;
+import com.kh.elefont.font.model.vo.Font;
+
 public class FontService {
+	private FontDao fontDao = new FontDao();
+
+	public int insertFont(Font font) {
+		Connection conn = getConnection();
+		int result = 0;
+		
+		try {
+			result = fontDao.insertFont(conn, font);
+			System.out.println("result@service = " + result);
+			
+			String fontNo = fontDao.selectLastFontNo(conn); 
+			System.out.println("fontNo@service = "+fontNo);
+			
+			font.setFontNo(fontNo);
+			
+			//attachment테이블 행 추가
+			Attachment attach = font.getAttach();
+			if(attach != null) {
+				attach.setFontNo(fontNo);
+				result = fontDao.insertAttachment(conn, attach);
+			}
+			commit(conn);
+		}catch(Exception e) {
+			e.printStackTrace();
+			rollback(conn);
+		}
+		close(conn);
+		return result;
+	}
 
 }
