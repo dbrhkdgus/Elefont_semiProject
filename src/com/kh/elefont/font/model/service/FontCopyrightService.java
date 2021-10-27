7657cf1d37771d2ccd507f7ef1ea79729f6aa46a
ehqkr <ehqkr@LAPTOP-1G18ANL7>
2021-10-27 11:16:10 +0900

    유광현 - fontCopyright 인서트 기능 추가 @ SellerFontEnrollServlet

--- src/com/kh/elefont/font/model/service/FontCopyrightService.java
+++ src/com/kh/elefont/font/model/service/FontCopyrightService.java
@@ -1,5 +1,31 @@
 package com.kh.elefont.font.model.service;
-import static com.kh.elefont.common.JdbcTemplate.*;
+import static com.kh.elefont.common.JdbcTemplate.close;
+import static com.kh.elefont.common.JdbcTemplate.commit;
+import static com.kh.elefont.common.JdbcTemplate.getConnection;
+import static com.kh.elefont.common.JdbcTemplate.rollback;
+
+import java.sql.Connection;
+
+import com.kh.elefont.font.model.dao.FontCopyrightDao;
+import com.kh.elefont.font.model.vo.FontCopyright;
 public class FontCopyrightService {
+private FontCopyrightDao fontCopyrightDao = new FontCopyrightDao();
+
+	public int insertFontCopyright(String fontNo, FontCopyright fontCopyright) {
+		Connection conn = getConnection();
+		int result = 0;
+		
+		try {
+			result = fontCopyrightDao.insertFontCopyright(conn, fontNo, fontCopyright);
+			
+			commit(conn);
+		}catch(Exception e) {
+			e.printStackTrace();
+			rollback(conn);
+		}finally {
+			close(conn);
+		}
+		return result;
+	}
 
 }
