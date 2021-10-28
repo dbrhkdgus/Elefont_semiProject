669d2b0266acf818647aeaab24d04c5671901922
DarciKim <greendory.k@gmail.com>
2021-10-28 16:39:39 +0900

    김다현 - 프로필 사진 업로드 작업 중 28일 16:40

---
+++ src/com/kh/elefont/member/controller/proFileFileUploadServelt.java
@@ -0,0 +1,34 @@
+package com.kh.elefont.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class proFileFileUploadServelt
+ */
+@WebServlet("/member/profileFileUpload")
+public class proFileFileUploadServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("잘도착햇나요?");
+		String memberNo = request.getParameter("memberNo");
+		System.out.println(memberNo);
+	}
+
+}
