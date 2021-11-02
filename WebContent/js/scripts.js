596d3b9bf96f30d48127b484457e80a729193a10
ehqkr <ehqkr@LAPTOP-1G18ANL7>
2021-11-02 16:48:36 +0900

    유광현 = 메인랜딩 고양이만 날아다녀.... 1648

--- WebContent/js/scripts.js
+++ WebContent/js/scripts.js
@@ -153,23 +153,7 @@
 
        
 	   
-/* Testimonial Carousel/Slider */
 
-/*$(".testimonial-carousel-list").owlCarousel({
-    items: 1,
-    autoPlay: true,
-    stopOnHover: false,
-    navigation: true,
-    navigationText: ["<i class='fa fa-long-arrow-left fa-2x owl-navi'></i>", "<i class='fa fa-long-arrow-right fa-2x owl-navi'></i>"],
-    itemsDesktop: [1199, 1],
-    itemsDesktopSmall: [980, 1],
-    itemsTablet: [768, 1],
-    itemsTabletSmall: false,
-    itemsMobile: [479, 1],
-    autoHeight: true,
-    pagination: false,
-    transitionStyle : "backSlide"
-});*/
 		
 		
 		
