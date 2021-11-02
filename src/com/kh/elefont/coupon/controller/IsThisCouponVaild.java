0d3d71452d587aaccf54a25c1c67d639e8029e87
DarciKim <greendory.k@gmail.com>
2021-11-02 16:35:37 +0900

    김다현 - 작업중 16:35

--- src/com/kh/elefont/coupon/controller/IsThisCouponVaild.java
+++ src/com/kh/elefont/coupon/controller/IsThisCouponVaild.java
@@ -1,11 +1,16 @@
 package com.kh.elefont.coupon.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.elefont.coupon.model.service.CouponService;
+import com.kh.elefont.coupon.model.vo.Coupon;
 
 /**
  * Servlet implementation class IsThisCouponVaild
@@ -14,13 +19,26 @@ import javax.servlet.http.HttpServletResponse;
 public class IsThisCouponVaild extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	CouponService couponService = new CouponService();
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String memberNo = request.getParameter("memberNoToReg");
-		System.out.println("비동기로 쿠폰 있는 지 확인하기 위한 memberNo : " + memberNo);
 		
+		
+		String memberNo = request.getParameter("memberNoToReg");
+		String memberId = request.getParameter("memberIdToReg");
+		System.out.println("비동기로 쿠폰 있는 지 확인하기 위한 memberNo : " + memberNo);
+		System.out.println("비동기로 쿠폰 있는 지 확인하기 위한 memberId : " + memberId);
+		
+		String couponNo1 = request.getParameter("coupon-no1");
+		String couponNo2 = request.getParameter("coupon-no2");
+		String couponNo3 = request.getParameter("coupon-no3");
+		System.out.println("couponNo1 : " + couponNo1);
+		System.out.println("couponNo2 : " + couponNo2);
+		System.out.println("couponNo3 : " + couponNo3);
+		
+		List<Coupon> couponList = couponService.selectAllCouponByMemberNo(memberNo);
 		
 	}
 
