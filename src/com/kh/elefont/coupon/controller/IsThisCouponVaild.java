79e1c9947f11da060584bb6a805b60037f445d75
DarciKim <greendory.k@gmail.com>
2021-11-02 17:37:58 +0900

    김다현 - 사용자 쿠폰 등록 비동기 작업중  11월02일 17:37

--- src/com/kh/elefont/coupon/controller/IsThisCouponVaild.java
+++ src/com/kh/elefont/coupon/controller/IsThisCouponVaild.java
@@ -34,11 +34,36 @@ public class IsThisCouponVaild extends HttpServlet {
 		String couponNo1 = request.getParameter("coupon-no1");
 		String couponNo2 = request.getParameter("coupon-no2");
 		String couponNo3 = request.getParameter("coupon-no3");
+		
 		System.out.println("couponNo1 : " + couponNo1);
 		System.out.println("couponNo2 : " + couponNo2);
 		System.out.println("couponNo3 : " + couponNo3);
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(couponNo1);
+		sb.append("-");
+		sb.append(couponNo2);
+		sb.append("-");
+		sb.append(couponNo3);
+		
+		System.out.println("쿠폰 연결한 값 " + sb);
+		
+		String sbtoString = sb.toString();
+		
+		System.out.println("sbtoString : " + sbtoString);
+		
 		
 		List<Coupon> couponList = couponService.selectAllCouponByMemberNo(memberNo);
+		System.out.println("couponList " + couponList );
+		
+		for(Coupon coupon : couponList) {
+			String couponNumber = coupon.getCouponNo();
+			System.out.println("couponNumber는 " + couponNumber );
+
+			if(sbtoString.equals(couponNumber)) {
+				System.out.println(sbtoString + "이 쿠폰은 사용할 수 있는 쿠폰입니다");
+			}
+		}
 		
 	}
 
