61a27c609df8447f6c480ee35fc9702efd53f687
DarciKim <greendory.k@gmail.com>
2021-11-02 16:02:05 +0900

    김다현 - 작업중

---
+++ src/com/kh/elefont/coupon/controller/IsThisCouponVaild.java
@@ -0,0 +1,25 @@
+package com.kh.elefont.coupon.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IsThisCouponVaild
+ */
+@WebServlet("/coupon/isThisCouponVaild")
+public class IsThisCouponVaild extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberNo = request.getParameter("memberNoToReg");
+		System.out.println("비동기로 쿠폰 있는 지 확인하기 위한 memberNo : " + memberNo);
+	}
+
+}
